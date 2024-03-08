1dfafabc4f91edd5869155fad9677f83a5a784a2
Mohamed Essebaiy <ahmadessebaiy@gmail.com>
2024-03-08 16:46:44 +0100

    Create Class Expenses and Expense with Dependency Uuid.

---
+++ lib/expenses.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Expenses'),),
+      body: const Center(
+        child: Column(
+          children: [
+            Text('.....'),
+            Text('.....'),
+          ],
+        ),
+      ),
+    );
+  }
+}
